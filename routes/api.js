4a6696b5c4cd5e31d43eb7238c44a34f9f5f9094
Subrata Sarker <subrata_sarker@yahoo.com>
2019-12-05 12:24:05 +0900

    Pass option { useUnifiedTopology: true } to the mongoClient

--- routes/api.js
+++ routes/api.js
@@ -12,40 +12,42 @@ var expect = require('chai').expect;
 var MongoClient = require('mongodb');
 var ObjectId = require('mongodb').ObjectID;
 
-const CONNECTION_STRING = process.env.DB; 
-MongoClient.connect(CONNECTION_STRING, function(err, connection) {
-  if (err) {
-    console.log('Connection to DB failed!');
-  }
-  else if (connection) {
-    console.log('Connection to DB successful!');
-    var db = connection.db('issutracker');
+const CONNECTION_STRING = process.env.DB;
+MongoClient.connect(CONNECTION_STRING,
+  { useUnifiedTopology: true },
+  function (err, connection) {
+    if (err) {
+      console.log('Connection to DB failed!');
+    }
+    else if (connection) {
+      console.log('Connection to DB successful!');
+      var db = connection.db('issutracker');
 
-  }
-});
+    }
+  });
 
 module.exports = function (app) {
 
   app.route('/api/issues/:project')
-  
-    .get(function (req, res){
+
+    .get(function (req, res) {
       var project = req.params.project;
-      
+
     })
-    
-    .post(function (req, res){
+
+    .post(function (req, res) {
       var project = req.params.project;
-      
+
     })
-    
-    .put(function (req, res){
+
+    .put(function (req, res) {
       var project = req.params.project;
-      
+
     })
-    
-    .delete(function (req, res){
+
+    .delete(function (req, res) {
       var project = req.params.project;
-      
+
     });
-    
+
 };
